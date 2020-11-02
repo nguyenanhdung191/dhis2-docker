1be645140fe603bae61e3993acff2dfb0e8981ca
Nguyễn Anh Dũng <nguyenanhdung191@gmail.com>
2020-11-02 14:03:48 +0700

    push

--- dhis2/setenv.sh
+++ dhis2/setenv.sh
@@ -1,3 +1,3 @@
-export JAVA_HOME='/usr/lib/jvm/java-1.11.0-openjdk-amd64/'
-export JAVA_OPTS='-Xmx7500m -Xms4000m'
+export JAVA_HOME='/usr/lib/jvm/java-1.11.0-openjdk-amd64/' 
+export JAVA_OPTS='-Xmx7500m -Xms4000m' 
 export DHIS2_HOME='/home/dhis2/config'
